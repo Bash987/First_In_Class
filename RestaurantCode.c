55f6efc795a6afe7a2cf89ce684f0a1cfecde0fa
Isaiah <iazelayandia@cpp.edu>
2022-03-02 18:51:34 -0800

    First if statement completed

--- RestaurantCode.c
+++ RestaurantCode.c
@@ -1,29 +1,31 @@
-// #include <stdio.h>
-// include library for time of day to seed random number generator <time.h>
+#include <stdio.h>
+#include <time.h>
 
-// int main()
+int main()
+{
 
-    //time_t time;
-    //int randomNum;
-    //float mealCost, 
-    //float tip,
-    //float tax,
-    //float mealTax,
-    //float mealTip,
-    //float mealTotal;
+    time_t t;
+    int randomNum;
+    float mealCost, 
+          tip,
+          tax,
+          mealTax,
+          mealTip,
+          mealTotal;
 
-    //srand((unsigned) time(&t)) initialize random varaible
+    srand((unsigned) time(&t)); //initialize random varaible
 
-    //randomNUm = rand() % 4
-    // if random = 0
-        //mealCost = 9.95
-        //printF("Your pizza cost is $%f, please input tax percentage (first) a space and tip percentage (second value) ", mealCost)
-        //scanf("%f %f", &tax, &tip)
-        //mealTax = mealcost * (tax/100);
-        //mealTip = mealcost * (tax/100);
-        //mealTotal = mealCost + mealTax + mealTip
-        //printf("Tax on the meal is: %f; The Tip on your meal is: %f\n Total Price comes out to: %f", &mealTax, &mealTip, &mealTotal )
-
+    randomNum = rand() % 4;
+    if (randomNum == 0)
+    {
+        mealCost = 9.95;
+        printf("Your pizza cost is $%f, please input tax percentage (first) a space and tip percentage (second value) ", mealCost);
+        scanf("%f %f", &tax, &tip);
+        mealTax = mealCost * (tax/100);
+        mealTip = mealCost * (tax/100);
+        mealTotal = mealCost + mealTax + mealTip;
+        printf("Tax on the meal is: %f; The Tip on your meal is: %f\n Total Price comes out to: %f", &mealTax, &mealTip, &mealTotal);
+    }
     //else if randomNum = 1
         //mealCost = 4.55
     
@@ -33,4 +35,5 @@
     //else if randomNum = 3
         //mealCost = 13.25
     
-    //return 0;
+    return 0;
+}
