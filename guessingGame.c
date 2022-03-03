bb7b22ec08874b83461cb33327bf015dea28c4c5
Isaiah <iazelayandia@cpp.edu>
2022-03-02 23:55:29 -0800

    changes to guessingGame

--- guessingGame.c
+++ guessingGame.c
@@ -2,15 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-int menu()
-{
-    int menuInt;
-    printf(" Press 1 to play a game\nPress 2 to change the max number\nPress 3 to quit");
-    scanf("%d", menuInt);
-
-    return menuInt;
-}
-
 int main()
 {
     time_t t;
@@ -18,47 +9,51 @@ int main()
     int guess;
     int setMax = 10; 
     int entriesNum = 0;
+    int menuInt;
     char *record[500];
-
-
 
     srand((unsigned) time(&t)); //initialize random varaible
 
     randomNum = (rand() % setMax) +1;
 
-    menu();
+    printf(" Press 1 to play a game\nPress 2 to change the max number\nPress 3 to quit\n");
+    scanf("%d", menuInt);
 
-    if (menu() == 1)
+
+    if (menuInt == 1)
     {
-        if (guess = randomNum)
+        if (guess = 113)
+        {
+            printf("You have quit the game");
+            printf(" Press 1 to play a game\nPress 2 to change the max number\nPress 3 to quit\n");
+            scanf("%d", menuInt);
+        }
+
+        else if (guess = randomNum)
         {
             printf("You won!!! Congrats");
-            randomNum = (rand() % setMax) +1;
+            record[entriesNum]= "true";
             entriesNum++;
-            menu();
-
-
+            randomNum = (rand() % setMax) +1;
             }
         
         do 
             {
                 printf("Guess the number: ");
 
-                scanf("%d", guess);
+                scanf("%d", &guess);
 
                 if(guess > randomNum)
                 {
                     printf("This guess is too high, try again.\n");
-                    char result[] = "false";
-                    record[entriesNum] = *result;
+                    record[entriesNum] = "false";
                     entriesNum++;
                 }
 
                 else if(guess < randomNum)
                 {
                     printf("This guess is too low, try again.\n");
-                    char result[] = "false";
-                    record[entriesNum]= *result;
+                    record[entriesNum]= "false";
                     entriesNum++;
 
                 }
@@ -67,10 +62,10 @@ int main()
         while (guess != randomNum);
     }
 
-    else if (menu() == 2)
+    else if (menuInt == 2)
     {
         printf("Change the maximum numbers to guess");
-        scanf("%d", setMax);
+        scanf("%d", &setMax);
 
         if (setMax < 0 && setMax > 11)
         {
@@ -80,10 +75,30 @@ int main()
         do
         {
             printf("Your input is either too large or too small, please enter a number in our range:\n");
-            scanf("%d", setMax);
+            scanf("%d", &setMax);
         } while (setMax > 10 | setMax < 1 );
         
     }
+
+    else if (menuInt == 3)
+    {
+        for (int point = 0; point < entriesNum; point++)
+        { 
+            int count = 0; 
+
+            while(*(record[entriesNum]+count) != '\0') 
+            { 
+                printf("%c",*(record[entriesNum]+count++)); 
+            } 
+        printf("\n"); 
+        }    
+    }
+    else
+    {
+        printf("invalid number type in again");
+        scanf("%d", menuInt);
+    }
+    return 0;
 }
 
 
