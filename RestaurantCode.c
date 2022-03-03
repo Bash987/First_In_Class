ea1100500790fe6a89d00ee1aa4a43c598b1f671
Isaiah <iazelayandia@cpp.edu>
2022-03-02 18:39:31 -0800

    Outline of RestaurnatCode

---
+++ RestaurantCode.c
@@ -0,0 +1,36 @@
+// #include <stdio.h>
+// include library for time of day to seed random number generator <time.h>
+
+// int main()
+
+    //time_t time;
+    //int randomNum;
+    //float mealCost, 
+    //float tip,
+    //float tax,
+    //float mealTax,
+    //float mealTip,
+    //float mealTotal;
+
+    //srand((unsigned) time(&t)) initialize random varaible
+
+    //randomNUm = rand() % 4
+    // if random = 0
+        //mealCost = 9.95
+        //printF("Your pizza cost is $%f, please input tax percentage (first) a space and tip percentage (second value) ", mealCost)
+        //scanf("%f %f", &tax, &tip)
+        //mealTax = mealcost * (tax/100);
+        //mealTip = mealcost * (tax/100);
+        //mealTotal = mealCost + mealTax + mealTip
+        //printf("Tax on the meal is: %f; The Tip on your meal is: %f\n Total Price comes out to: %f", &mealTax, &mealTip, &mealTotal )
+
+    //else if randomNum = 1
+        //mealCost = 4.55
+    
+    // else if randomNUm = 2
+        //mealCost = 13.25
+
+    //else if randomNum = 3
+        //mealCost = 13.25
+    
+    //return 0;
