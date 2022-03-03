9cab431db87a6eecfdc7058df24878cb30217f3a
Isaiah <iazelayandia@cpp.edu>
2022-03-02 22:20:07 -0800

    making outline

---
+++ guessingGame.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//int menu ()
+/*{
+    int menuInt;
+    //printf(" Press 1 to play a game\nPress 2 to change the max number\nPress 3 to quit")
+    //scanf("%d", menu)
+
+    return menuInt;
+    } */
+
+// int main()
+
+    //time_t time;
+    //int randomNum;
+    //int guess;
+    //int setMax = 10; 
+    //char *[]
+
+
+
+    //srand((unsigned) time(&t)); initialize random varaible
+
+    //randomNum = rand() % setMax;
+
+    //menu();
+
+    //if (menu() == 1)
+        //if (guess = randomNum)
+        /*{
+            printf("You won!!! Congrats");
+            randomdomNum = rand() % 4
+            menu();
+
+            }*/
+        
+        //do 
+            /*{
+                printf("guess the number");
+
+                scanf("%d", guess)
+
+
+
+                } */
+        //while (guess != randomNum);
+
+
+  
+
+
